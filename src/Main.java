efd2bb4df552ac2e40156724a8a020caa382d30e
Foxssj <nicanorgonzalez2003@gmail.com>
2023-03-28 10:43:11 +0100

    feat(): added user number

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un numero");
+        int number = sc.nextInt();
+        System.out.println("Hola mundo " + number + "!");
+        sc.close();
+
     }
 }
